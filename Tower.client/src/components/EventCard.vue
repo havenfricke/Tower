887b8eef51d8d3e81b2594e3c3555c0d474ed29b
Haven Fricke <robslaven@live.com>
2022-03-19 15:44:12 -0600

    homepage styling

---
+++ Tower.client/src/components/EventCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <img
+    class="img-fluid rounded-top"
+    src="https://www.em-normandie.com/sites/default/files/2019-12/msc-international-events-management.jpg"
+    alt=""
+  />
+  <div class="col-4 text-dark">
+    <h3>Event here</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
--- Tower.client/src/pages/HomePage.vue
+++ Tower.client/src/pages/HomePage.vue
@@ -1,11 +1,44 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo" class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
+  <div class="container-fluid">
+    <div class="row mt-5">
+      <div class="col-12 d-flex justify-content-center">
+        <div class="row d-flex justify-content-center">
+          <h1
+            class="col-12 p-3 img-fluid border border-info rounded"
+            style="
+              width: 90vw;
+              height: 60vh;
+              background-image: url('https://www.em-normandie.com/sites/default/files/2019-12/msc-international-events-management.jpg')
+              background-size: contain;
+            "
+          >
+            <p class="p-2 border-bottom border-light">Events for everyone 
+              <ul>
+              <li class="fs-4 ms-5 mt-4">Plan your events</li>
+              <li class="fs-4 ms-5 mt-4">Find events that interest you</li>
+              <li class="fs-4 ms-5 mt-4">Become a part of the communities you love</li>
+              <li class="fs-4 ms-5 mt-4">Set the world on fire</li>
+              <li class="fs-4 ms-5 mt-4">Just kidding, but your parties will be lit</li>
+              </ul>
+              </p>
+          </h1>
+        </div>
     </div>
+        <div class="row d-flex justify-content-center p-5">
+          <div class="col-12 fs-5 bg-secondary rounded p-1 mx-3">
+            <span class="row d-flex justify-content-around text-white p-3"><a class="col-3 text-center hoverable text-white">CONCERT</a><a class="col-3 text-center hoverable text-white">CONVENTION</a><a class="col-3 text-center hoverable text-white">SPORT</a><a class="col-3 text-center hoverable text-white">DIGITAL</a></span>
+          </div>
+        </div>
+        <div class="container">
+        <div class="row mx-4 my-3">
+          <div class="col-4 rounded bg-light hoverable p-1">
+            <EventCard />
+          </div>
+            
+            
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -16,15 +49,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.home{
+.home {
   display: grid;
   height: 80vh;
   place-content: center;
   text-align: center;
   user-select: none;
-  .home-card{
+  .home-card {
     width: 50vw;
-    > img{
+    > img {
       height: 200px;
       max-width: 200px;
       width: 100%;
@@ -32,5 +65,16 @@ export default {
       object-position: center;
     }
   }
+}
+
+.hoverable:hover {
+  transform: scale(1.09);
+  filter: drop-shadow(0px 15px 10px rgba(0, 0, 0, 0.3));
+  transition: 50ms ease-in-out;
+  cursor: pointer;
+}
+.hoverable:active {
+  transform: scale(0.98);
+  transition: 50ms ease-in-out;
 }
 </style>
